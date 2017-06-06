e638e0a71521ed88140d99c3e5faa45b100a0292
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-06 01:55:44 +0000

    Edit routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/about' => 'pages#about'
+
+  get '/contact' => 'pages#contact'
+  
+  root 'pages#home'
 end
