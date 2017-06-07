2fa8a0829c9e7b72fad799230b38c3b4f62225a6
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-07 04:19:14 +0000

    Configure db.yml to fix pgsql errors and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: 'Second Blog',
+    body: 'Everyday carry farm-to-table kale chips, selvage assumenda cloud bread 
+           forage fugiat mixtape. Pinterest DIY hell of duis kale chips, pitchfork 
+           adipisicing. Chillwave meh cornhole adipisicing, ugh fugiat occupy 
+           godard. Stumptown celiac pabst YOLO jean shorts humblebrag, scenester 
+           meh heirloom commodo raw denim yr pork belly beard forage. Ea quinoa 
+           occupy yuccie vero qui, kinfolk disrupt anim. Master cleanse kombucha 
+           pinterest dolor banh mi. Est enamel pin letterpress, man bun try-hard 
+           salvia migas typewriter fingerstache.'
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 60
+  )
+end
+
+puts "5 skills created"
+
+
+6.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My portfolio item",
+    body: "Everyday carry farm-to-table kale chips, selvage assumenda cloud bread 
+           forage fugiat mixtape. Pinterest DIY hell of duis kale chips, pitchfork 
+           adipisicing. Chillwave meh cornhole adipisicing, ugh fugiat occupy 
+           godard. Stumptown celiac pabst YOLO jean shorts humblebrag, scenester 
+           meh heirloom commodo raw denim yr pork belly beard forage. Ea quinoa 
+           occupy yuccie vero qui, kinfolk disrupt anim. Master cleanse kombucha 
+           pinterest dolor banh mi. Est enamel pin letterpress, man bun try-hard 
+           salvia migas typewriter fingerstache.",
+    main_image: "http://via.placeholder.com/700x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+
+
+
+puts "9 portfolio items created"
