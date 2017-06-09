fb573240eda040e5696dcd05cff095195dbee297
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-09 23:54:37 +0000

    Add route and method to toggle post status

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id' => 'portfolios#show', as: 'show_portfolio'
   
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
   get '/about' => 'pages#about'
 
