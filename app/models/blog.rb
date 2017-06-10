d32b3e8f222d67464cec0a061c420d91f0d4fac1
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-10 21:55:30 +0000

    Add validations for blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+  
+  validates :title, presence: true, length: { in: 10..150 }
+  validates :body,  presence: true, length: { in: 100..1000 }
 end
