94a121fe27feb49266f9a914bc33359187f94cd8
Faisal Hassan <faisal.hassanx@gmail.com>
2017-06-11 21:50:28 +0000

    Edit db sed file and add associations between topic and blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog: #{blog}",
@@ -8,7 +16,8 @@
            meh heirloom commodo raw denim yr pork belly beard forage. Ea quinoa 
            occupy yuccie vero qui, kinfolk disrupt anim. Master cleanse kombucha 
            pinterest dolor banh mi. Est enamel pin letterpress, man bun try-hard 
-           salvia migas typewriter fingerstache.'
+           salvia migas typewriter fingerstache.',
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,10 +33,27 @@ end
 puts "5 skills created"
 
 
-6.times do |portfolio_item|
+4.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My portfolio item",
+    subtitle: "Ruby on Rails",
+    body: "Everyday carry farm-to-table kale chips, selvage assumenda cloud bread 
+           forage fugiat mixtape. Pinterest DIY hell of duis kale chips, pitchfork 
+           adipisicing. Chillwave meh cornhole adipisicing, ugh fugiat occupy 
+           godard. Stumptown celiac pabst YOLO jean shorts humblebrag, scenester 
+           meh heirloom commodo raw denim yr pork belly beard forage. Ea quinoa 
+           occupy yuccie vero qui, kinfolk disrupt anim. Master cleanse kombucha 
+           pinterest dolor banh mi. Est enamel pin letterpress, man bun try-hard 
+           salvia migas typewriter fingerstache.",
+    main_image: "http://via.placeholder.com/700x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+2.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Everyday carry farm-to-table kale chips, selvage assumenda cloud bread 
            forage fugiat mixtape. Pinterest DIY hell of duis kale chips, pitchfork 
            adipisicing. Chillwave meh cornhole adipisicing, ugh fugiat occupy 
